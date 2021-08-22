e89b0738a8fd7f600d507e953613639b46ccadc1
xiangzheng <18896101294@163.com>
2021-08-22 19:32:19 +0800

    feat： 请求拦截

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,6 @@
 import Cookies from 'js-cookie'
 
-const TokenKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI0OWRmMTYwMi1mNWYzLTRkNTItYWZiNy0zODAyZGE2MTk1NTgiLCJhY2NvdW50IjoiYWRtaW4iLCJuYmYiOjE2MjkzNjU0MjUsImV4cCI6MTYzMDIyOTQyNSwiaXNzIjoiWHouTm9kZSIsImF1ZCI6Inh6In0.drYOl5cDIhyXErCTyDfuJ41Kitqs8RYBgArZTEtMHxI'
+const TokenKey = 'XzNode-Token'
 
 export function getToken() {
   return Cookies.get(TokenKey)
--- src/utils/request.js
+++ src/utils/request.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { MessageBox, Message } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
+import router from "@/router";
 
 // create an axios instance
 const service = axios.create({
@@ -19,7 +20,7 @@ service.interceptors.request.use(
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers['X-Token'] = getToken()
+      // config.headers['X-Token'] = getToken()
       config.headers.Authorization = 'Bearer ' + getToken()
     }
     return config
@@ -45,23 +46,19 @@ service.interceptors.response.use(
    */
   response => {
     const res = response.data
-
-    return res;
-
     // if the custom code is not 20000, it is judged as an error.
-    if (res.code !== 20000) {
+    if (res.code !== 200) {
       Message({
         message: res.message || 'Error',
         type: 'error',
         duration: 5 * 1000
       })
-
       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+      if (res.code === 401) {
         // to re-login
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
+        MessageBox.confirm('您已注销，可以取消以停留在此页面，或重新登录', '确认注销', {
+          confirmButtonText: '重新登录',
+          cancelButtonText: '取消',
           type: 'warning'
         }).then(() => {
           store.dispatch('user/resetToken').then(() => {
